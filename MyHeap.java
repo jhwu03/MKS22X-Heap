99b8eedc27705c06e2208a5bb1459e704e6c59b9
Jionghao <jwu03@stuy.edu>
2019-04-30 11:46:06 -0400

    adding functions

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,23 @@
 public class MyHeap{
+     // /We discussed these 2 methods already:
+     // - size  is the number of elements in the data array.
+     // - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
+     // - precondition: index is between 0 and size-1 inclusive
+     // - precondition: size is between 0 and data.length-1 inclusive.
+     private static void pushDown(int[]data,int size,int index){
+
+     }
+
+     public static void heapify(int[])
+    //- convert the array into a valid heap. [ should be O(n) ]
+
+    public static void heapsort(int[])
+    //- sort the array [ should be O(nlogn) ] :
+    // converting it into a heap
+    // removing the largest value n-1 times (remove places at end of the sub-array).
+
+
+
+
 
 }
