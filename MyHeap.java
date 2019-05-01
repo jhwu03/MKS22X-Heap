f599f6fe5209e70218b35a9b7ed9f701cd253944
jwu03 <jwu03@stuy.edu>
2019-04-30 20:48:59 -0400

    added pushup

--- MyHeap.java
+++ MyHeap.java
@@ -4,7 +4,7 @@ public class MyHeap{
      // - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
      // - precondition: index is between 0 and size-1 inclusive
      // - precondition: size is between 0 and data.length-1 inclusive.
-     private static void pushDown(int[]data,int size,int index){
+private static void pushDown(int[]data,int size,int index){
        boolean status = false;
        while(!status){
        child1 = (index * 2) + 1;
@@ -38,7 +38,34 @@ public class MyHeap{
          }else status = done;
        }
      }
+   }
+
+//  /**A method that pushes an element at a given index upward
+// *@param int[] data
+// *@param int index
+// - push the element at index i up into the correct position. This will swap it with the parent node until the parent node is larger or the root is reached. [ should be O(logn) ]
+// - precondition: index is between 0 and data.length-1 inclusive.
+// */
+ private static void pushUp(int[]data, int index){
+   boolean status = false;
+   while(!status){
+     if(index == 0) done = true;
+     else{
+       int parent;
+       if(index % 2 == 0) parent = (index / 2) - 1;
+       else parent = index / 2;
+       if(parent >= 0){
+         int temp = data[index];
+         if(data[index] >= data[parent]){
+           data[index] = data[parent];
+           data[parent] = temp;
+           index = parent;
+         }else status= true;
+       }else status = true;
      }
+   }
+ }
+
 
      public static void heapify(int[])
     //- convert the array into a valid heap. [ should be O(n) ]
