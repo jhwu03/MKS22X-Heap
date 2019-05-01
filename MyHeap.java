0ad9def30b786186d16892223a9bf930a8f91f16
jwu03 <jwu03@stuy.edu>
2019-04-30 20:25:54 -0400

    added pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -5,10 +5,39 @@ public class MyHeap{
      // - precondition: index is between 0 and size-1 inclusive
      // - precondition: size is between 0 and data.length-1 inclusive.
      private static void pushDown(int[]data,int size,int index){
+       boolean status = false;
+       while(!status){
        child1 = (index * 2) + 1;
        child2 = (index * 2) + 2;
-       if(child1 > size || child2 > size )return;
-       if(child1 <= size && child2 <= size)
+       if(child1 >= size && child2 >= size ) status = true;
+       if(child1 < size && child2 < size){
+         int max = 0;
+         if(data[child1] > data[child2]){
+           max = Math.max(data[child1],data[index]);
+         }else{
+           max = Math.max(data[child2],data[index]);
+         }
+         if(max != data[index]){
+           if(max == data[child1]){
+             data[child1] = data[index];
+             data[index] = max;
+             index = child1;
+           }else{
+             data[child2] = data[index];
+             data[index] = max;
+             index = child2;
+           }
+         }else status = true;
+       }else if(child1 < size){
+         int max = 0;
+         max = Math.max(data[child1],data[index]);
+         if(max != data[index]){
+           data[child1] = data[index];
+           data[index] = max;
+           index = child1;
+         }else status = done;
+       }
+     }
      }
 
      public static void heapify(int[])
